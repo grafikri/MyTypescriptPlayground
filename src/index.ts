43e709dcd6762e422ac216f3b8d9ba19b7796f4e
Serhan Coşkun <grafikri@gmail.com>
2020-02-17 23:37:16 +0300

    search algorithm moved to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,114 @@
-let arr = [
-  [[4, 8], [0, 0], [0, 0], [0, 0]],
-  [[1, 5], [0, 0], [0, 0], [4, 8]],
-  [[3, 4], [0, 0], [0, 0], [4, 8]],
-  [[1, 1], [0, 0], [0, 0], [4, 8]]
-]
+// const replacement = new ReplacementText({ language: "tr" })
+// replacement.setTag()
+// const result = replacement.replace(text, search)
 
-function transpose(arr: any){
+import transpose from "transpose-2d-array"
 
-  let arrTranspose:any[][] = []
+class ReplacementText {
+  public options = {}
 
-  for (let i = 0;i < arr.length;i++) {
-    const subArr = arr[i]
-    arrTranspose[i] = []
-    for (let j = 0;j < subArr.length;j++) {
-      
-      const item = subArr[j]
-      arrTranspose[j][i] = item
+  constructor(incomingOptions = {}) {
+    const defaultOptions = {
+      language: "tr",
+      tag: "span"
+    }
 
-      //console.log("item: ", item);
-      // console.log("total: ", total);
-      // console.log("positions: ", positions[i]);
+    this.options = { ...defaultOptions, ...incomingOptions }
+  }
+
+  replace(text: string, search: string) {
+    const arr = this.findSearchIndexes(text, search)
+    const { arrText, arrSearch } = arr
+    const positions = this.detectIndexes(arrText, arrSearch)
+    const arrTranspose = transpose(positions)
+    const results = this.determinePositions(arrTranspose)
+    const modifiedText = this.replacedText(text.split(" "), results)
+    return modifiedText.join(" ")
+  }
+
+  private findSearchIndexes(text: string, search: string) {
+    const arrText: string[] = text.toLocaleLowerCase("tr").split(" ")
+    const arrSearch: string[] = search.toLocaleLowerCase("tr").split(" ")
+
+    return {
+      arrText,
+      arrSearch
     }
   }
 
-  console.log(arrTranspose);
+  private detectIndexes(arrText: string[], arrSearch: string[]) {
+    let positions: any[] = []
+    for (let i = 0; i < arrSearch.length; i++) {
+      const searhingItem = arrSearch[i]
+      positions[i] = []
+      for (let j = 0; j < arrText.length; j++) {
+        let textItem = arrText[j]
+        let index = textItem.indexOf(searhingItem)
+
+        if (index !== -1) {
+          const firstPosition = index
+          const lastPosition = index + searhingItem.length
+          positions[i].push([firstPosition, lastPosition])
+        } else {
+          positions[i].push([0, 0])
+        }
+      }
+    }
+
+    return positions
+  }
+
+  determinePositions(arr: any[]) {
+    let positions = []
+
+    for (let i = 0; i < arr.length; i++) {
+      const subArr = arr[i]
+      for (let j = 0; j < subArr.length; j++) {
+        const item = subArr[j]
+        const total = item[0] + item[1]
+
+        if (positions[i] !== undefined) break
+
+        if (total > 0) {
+          positions[i] = item
+          break
+        }
+
+        const isLastItem = subArr.length - 1 === j
+
+        if (isLastItem && total === 0) {
+          positions[i] = item
+          break
+        }
+      }
+    }
+
+    return positions
+  }
+
+  replacedText(
+    arrText: any[],
+    arrPosition: any[],
+    tagName = "span",
+    className = "bold"
+  ) {
+    return arrText.map((item, index) => {
+      let firstPosition = arrPosition[index][0]
+      let lastPosition = arrPosition[index][1]
+      if (firstPosition == 0 && lastPosition == 0) {
+        return item
+      } else {
+        return (
+          item.substr(0, firstPosition) +
+          `<${tagName} class="${className}">` +
+          item.substr(firstPosition, lastPosition) +
+          `</${tagName}>` +
+          item.substr(lastPosition, item.length - lastPosition)
+        )
+      }
+    })
+  }
 }
 
-transpose(arr)
-
-// function detectPositions(arr: any){
-//   let size = arr[0].length
-//   let positions = []
-
-//   for(let i=0; i < arr.length; i++){
-//     const subArr = arr[i]
-//     if (positions[i] !== undefined) break
-//     for (let j = 0;j < subArr.length;j++) {
-      
-
-//       const item = subArr[j]
-//       const total = item[0] + item[1]
-      
-//       if (total > 0) {
-//         console.log("rank item: ", item);
-//         console.log("positions: ", positions[i], i);
-//         positions[i] = item
-//       }
-      
-//       //console.log("item: ", item);
-//       // console.log("total: ", total);
-//       // console.log("positions: ", positions[i]);
-//     }
-//   }
-
-//   return positions
-  
-// }
-
-// console.log(detectPositions(arr))
-
-
-
-// function transpose(arr: any) {
-
-//   let transposeArr: any[][]
-//   transposeArr = []
-
-//   arr.reduce((first: any, subItems: any, firstIndex: number) => {
-//     transposeArr[firstIndex] = []
-//     subItems.reduce((second: any, item: any, index: number) => {
-//       //transposeArr[index][firstIndex] = item
-
-//       //console.log("item: ", item)
-//       // console.log("index: ", index)
-//     }, [])
-
-//   }, [])
-
-//   return transposeArr
-// }
-
-// console.log(transpose(arr));
-// //console.log("arr: ", );
+const engine = new ReplacementText()
+const result = engine.replace("Bugün hava çok güzel", "Bugün güzel ava")
